fdbe0d1122fc854c64e7ca58574576e03f414af0
Siti Arfahanis Suradi <74125099+arfaluvess@users.noreply.github.com>
2022-10-22 11:54:40 +0800

    Update myfirst.py

--- myfirst.py
+++ myfirst.py
@@ -1,3 +1,9 @@
-import seaborn as sns
-iris = sns.load_dataset('iris')
-sns.boxplot(data=iris)
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.header("My first Streamlit App")
+st.write(pd.DataFrame({
+    'Intplan': ['yes', 'yes', 'yes', 'no'],
+    'Churn Status': [0, 0, 0, 1]
+}))
