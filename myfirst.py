cb2f69ae5a8b19681363aeca53809942b3e1f75b
Siti Arfahanis Suradi <74125099+arfaluvess@users.noreply.github.com>
2022-10-22 16:16:55 +0800

    Update myfirst.py

--- myfirst.py
+++ myfirst.py
@@ -5,10 +5,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 st.header("My first Streamlit App")
-st.write(pd.DataFrame({
-    'Intplan': ['yes', 'yes', 'yes', 'no'],
-    'Churn Status': [0, 0, 0, 1]
-}))
 
 iris = sns.load_dataset('iris')
 fig = plt.figure(figsize=(10, 4))
