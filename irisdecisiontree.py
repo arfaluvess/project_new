672b1918ae6daf66bc03651689e7115936e345d4
Siti Arfahanis Suradi <74125099+arfaluvess@users.noreply.github.com>
2022-10-23 11:45:59 +0800

    Update irisdecisiontree.py

--- irisdecisiontree.py
+++ irisdecisiontree.py
@@ -16,6 +16,8 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix 
 
+st.header("Machine Learning Application - SVM AND DECISION TREE")
+
 # SVM DATASET
 iris = sns.load_dataset('iris') # returns a pandas dataframe
 X_iris = iris.drop('species', axis=1)  
