6cc617ee847b5eb56ab1edc678833efd62bfb10f
Siti Arfahanis Suradi <74125099+arfaluvess@users.noreply.github.com>
2022-10-23 11:01:06 +0800

    Update irisdecisiontree.py

--- irisdecisiontree.py
+++ irisdecisiontree.py
@@ -2,6 +2,9 @@ from sklearn.datasets import load_iris
 from sklearn import tree
 from sklearn.tree import plot_tree
 
+iris = load_iris()
+(X_iris, y_iris) = load_iris(return_X_y = True)
+# X_train, X_test, y_train, y_test = train_test_split(X_iris, y_iris, random_state = 40)
 Xtrain, Xtest, ytrain, ytest = train_test_split(X_iris, y_iris,random_state=1)
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(Xtrain, ytrain)
