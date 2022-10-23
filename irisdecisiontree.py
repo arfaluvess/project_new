c8088e911ced7a7194233227e4c384ea4808f543
Siti Arfahanis Suradi <74125099+arfaluvess@users.noreply.github.com>
2022-10-23 11:49:26 +0800

    Update irisdecisiontree.py

--- irisdecisiontree.py
+++ irisdecisiontree.py
@@ -31,10 +31,10 @@ X_train, X_test, y_train, y_test = train_test_split(X_iris, y_iris, random_state
 
 
 clf = SVC(kernel='rbf', C=1).fit(X_train, y_train)
-print('Iris Flower Dataset')
-print('Accuracy of RBF SVC classifier on training set: {:.2f}'
+st.write('Iris Flower Dataset')
+st.write('Accuracy of RBF SVC classifier on training set: {:.2f}'
      .format(clf.score(X_train, y_train)))
-print('Accuracy of RBF SVC classifier on test set: {:.2f}'
+st.write('Accuracy of RBF SVC classifier on test set: {:.2f}'
      .format(clf.score(X_test, y_test)))
 
 # from sklearn.naive_bayes import GaussianNB # 1. choose model class
@@ -44,22 +44,22 @@ y_model = model.predict(X_test)             # 4. predict on new data
 y_model
 
 # Accuracy score
-accuracy_score(y_test, y_model)
+st.write(accuracy_score(y_test, y_model))
 
 # Classification report finding
-print(classification_report(y_test, y_model)) 
+st.write(classification_report(y_test, y_model)) 
 
 # Confusion Matrix
 confusion_matrix(y_test, y_model)
 
 #Confusion Matrix
 n_cm = metrics.confusion_matrix(y_test, y_model)
-print(n_cm)
+st.write(n_cm)
 
 cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = n_cm,display_labels=np.unique(y_iris))
-
 cm_display.plot()
-plt.show()
+fig=plt.figure(figsize=(15,8))
+plt.pyplot(fig)
 
 # F1 score = 2 / [ (1/precision) + (1/ recall)]
 st.write(classification_report(y_test, y_model)) 
