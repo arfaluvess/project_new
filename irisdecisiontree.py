35aba2699a3a90c310994d22dc7ce6fe6e4fb99b
Siti Arfahanis Suradi <74125099+arfaluvess@users.noreply.github.com>
2022-10-24 14:27:48 +0800

    Update irisdecisiontree.py

--- irisdecisiontree.py
+++ irisdecisiontree.py
@@ -17,8 +17,12 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix 
 from sklearn.metrics import plot_confusion_matrix
 
-st.header("DECISION TREE INFORMATION SUPERVISE ML")
+# Title and Subheader
+st.title("Iris EDA App")
+st.subheader("EDA Web App with Streamlit ")
 
+# PROBABILITY USING SVM 
+st.header('INFORMATION PROBABILITY USING SVM')
 # SVM DATASET
 iris = sns.load_dataset('iris') # returns a pandas dataframe
 X_iris = iris.drop('species', axis=1)  
@@ -39,36 +43,68 @@ st.write('Accuracy of RBF SVC classifier on test set: {:.2f}'
      .format(clf.score(X_test, y_test)))
 
 # from sklearn.naive_bayes import GaussianNB # 1. choose model class
-model = GaussianNB()                       # 2. instantiate model
-model.fit(X_train, y_train)                  # 3. fit model to data
-y_model = model.predict(X_test)             # 4. predict on new data
-y_model
+# model = GaussianNB()                       # 2. instantiate model
+# model.fit(X_train, y_train)                  # 3. fit model to data
+# y_model = model.predict(X_test)             # 4. predict on new data
+# y_model
 
-# Accuracy score
-st.write(accuracy_score(y_test, y_model))
+# # Accuracy score
+# st.write(accuracy_score(y_test, y_model))
 
-# Classification report finding
-st.write(classification_report(y_test, y_model)) 
+# # Classification report finding
+# st.write(classification_report(y_test, y_model)) 
 
-# Confusion Matrix
-confusion_matrix(y_test, y_model)
+# # Confusion Matrix
+# confusion_matrix(y_test, y_model)
 
-#Confusion Matrix
-n_cm = metrics.confusion_matrix(y_test, y_model)
-st.write(n_cm)
+# #Confusion Matrix
+# n_cm = metrics.confusion_matrix(y_test, y_model)
+# st.write(n_cm)
 
-cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = n_cm,display_labels=np.unique(y_iris))
-# fig, ax = plt.subplots()
+# cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = n_cm,display_labels=np.unique(y_iris))
+# # fig, ax = plt.subplots()
+# # cm_display.plot()
+# # st.pyplot(fig)
+
+# fig, ax = plt.subplots(figsize=(10,10))
+# st.write(sns.heatmap(iris.corr(), annot=True,linewidths=0.5))
 # cm_display.plot()
 # st.pyplot(fig)
 
+# # F1 score = 2 / [ (1/precision) + (1/ recall)]
+# st.write(classification_report(y_test, y_model)) 
+
+st.header("INFORMATION ON PROBABILITY USING SVM")
+
+# SVM METHOD 
+classifier = SVC(kernel = 'linear', random_state = 0)
+
+#Fit the model for the data
+classifier.fit(X_train, y_train)
+
+# FIND THE ACCURACY
+#Make the prediction
+y_pred = classifier.predict(X_test)
+
+cm = confusion_matrix(y_test, y_pred)
+st.write(cm)
+
+# Check for the accuracy
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+st.write("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+st.write("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
+
+# Show the plotting visualization
+cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = cm,display_labels=np.unique(y_iris))
+# cm_display.plot()
+# plt.show()
+
 fig, ax = plt.subplots(figsize=(10,10))
 st.write(sns.heatmap(iris.corr(), annot=True,linewidths=0.5))
-cm_display.plot()
-st.pyplot(fig)
+st.pyplot(cm_display.plot())
 
 # F1 score = 2 / [ (1/precision) + (1/ recall)]
-st.write(classification_report(y_test, y_model)) 
+st.write(classification_report(y_test, y_pred)) 
 
 st.header("INFORMATION ON DECISION TREE")
 
